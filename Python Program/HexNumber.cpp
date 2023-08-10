81a0af3e222b496be65a94ee70460a7c550d84e3
Kiranmayee Tallibiona <136068970+kiranmayeet@users.noreply.github.com>
2023-08-10 14:22:13 +0530

    HexNumber.cpp

---
+++ Python Program/HexNumber.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    int res = 2*pow(n,2)-n;
+    cout << res;
+    return 0;
+}
