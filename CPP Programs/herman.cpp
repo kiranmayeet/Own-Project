f5d4bbe06c4b0c13e185a1ed37c13880f32dd30a
Kiranmayee Tallibiona <136068970+kiranmayeet@users.noreply.github.com>
2023-08-10 14:38:05 +0530

    herman.cpp

---
+++ CPP Programs/herman.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main(){
+    int r;
+    cin >> r;
+    cout << fixed << setprecision(6) << M_PI*pow(r,2) << endl;
+    cout << fixed << setprecision(6) << 2*pow(r,2);
+}
