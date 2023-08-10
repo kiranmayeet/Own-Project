bbdd693e41b23fe59eaa4ace428f9f9ffc8a97c4
Kiranmayee Tallibiona <136068970+kiranmayeet@users.noreply.github.com>
2023-08-10 13:56:29 +0530

    Standardio3.cpp

---
+++ CPP Programs/Standardio3.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<iomanip>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << right << setw(3) << a << endl;
+    cout << right << setw(3) << b << endl;
+    cout << right << setw(3) << c << endl;
+    return 0;
+}
