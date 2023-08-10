9475fde25ed2b8b6be76019fd337586cf39251c9
Kiranmayee Tallibiona <136068970+kiranmayeet@users.noreply.github.com>
2023-08-10 14:01:39 +0530

    standardio1.java

---
+++ Java Programs/standardio1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1,n2,n3,sum;
+        n1 = sc.nextInt();
+        n2= sc.nextInt();
+        n3= sc.nextInt();
+        System.out.print(n1+n2+n3);
+    }
+}
